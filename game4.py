01be8349b267b1e72fa1393c9df3230214299527
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2021-10-28 23:05:53 +0700

    Update game4.py

--- game4.py
+++ game4.py
@@ -12,10 +12,10 @@ player_1 = 0 # vị trí tên lửa
 
 bullet_1 = 0 # vị trí của đạn
 bullet_2 = 370
-bullet1_change = 10 # tốc độ bắn ngang của đạn (nếu có)
+bullet1_change = 0 # tốc độ bắn ngang của đạn (nếu có)
 bullet2_change = 3 # tốc độ bắn lên của đạn
 bullet_state = "start" 
-# nếu là True thì auto bắn k cần space
+# nếu là True thì auto bắn k cần nhấn space
 
 screen = pygame.display.set_mode((Width, Height))
 pygame.display.set_caption("game")
@@ -49,32 +49,36 @@ while tuong: # vòng lặp
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 player_1 = -5
+                # tốc độ bay sang trái của tên lửa
             if event.key == pygame.K_RIGHT:
                 player_1 = 5
+                # tốc độ bay sang phải của tên lửa
             if event.key == pygame.K_SPACE:
                 fire_bullet(rocket_1, bullet_2)
 
-        if event.type == pygame.KEYUP:
+        if event.type == pygame.KEYUP: # xác định vị trí muốn dừng của tên lửa
             if event.key == pygame.K_LEFT:
                 player_1 = 0 
             if event.key == pygame.K_RIGHT:
                 player_1 = 0
 
     rocket_1 += player_1
-    if rocket_1 <= 0:
-        rocket_1 = 0
-    elif rocket_1 >=750:
-        rocket_1 = 750
+    if rocket_1 <= -15:
+        rocket_1 = -15
+#vị trí lề trái game mà tên lửa bay tới và đứng lại
+    elif rocket_1 >= 850:
+        rocket_1 = 850
+#vị trí lề phải game mà tên lửa bay tới và đứng lại
     
     if bullet_2 <= 0:
         bullet_2 = 400
         bullet_state = "start"
-        # nếu là True thì auto bắn k cần space
+    # nếu là True thì auto bắn k cần nhấn space
     if bullet_state is True:
         fire_bullet(rocket_1, bullet_2)
         bullet_2 -= bullet2_change 
 
-    rocket(rocket_1, rocket_2)
+    rocket(rocket_1, rocket_2)# cho tên lửa vô game
     pygame.display.update()
     print("game đang chạy") # đang trong vòng lặp 
 
